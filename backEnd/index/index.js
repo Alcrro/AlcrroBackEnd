bd4ef42f4bb37eec9954400b7bc4416203daeac8
Alcrro <alex.roventa94@gmail.com>
2022-11-05 16:14:36 +0200

    main

--- backEnd/index/index.js
+++ backEnd/index/index.js
@@ -1,9 +1,5 @@
-const server = require('express');
-
 server.get('/', (req, res, next) => {
   res.json({
     success: true,
   });
 });
-
-exports.module = server;
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const server = express();
 
+//Load en vars
+dotenv.config({ path: './config/configs.env' });
 
+const PORT = process.env.PORT || 5000;
+console.log(PORT);
 
-server.listen(5000);
+server.listen(PORT, console.log(`Server running in ${PORT}`));
