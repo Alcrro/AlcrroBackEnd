b9a269711850c725ef6b245ffd2109d1567b5e71
Alcrro <alex.roventa94@gmail.com>
2022-11-05 16:25:57 +0200

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,6 +4,6 @@ const server = express();
 
 //Load env vars
 dotenv.config({ path: './config/configs.env' });
-const PORT = process.env.DB_SERVER_PORT || 5000;
+const PORT = process.env.DB_SERVER_PORT;
 
 server.listen(PORT, console.log(`Server running in ${PORT}`));
