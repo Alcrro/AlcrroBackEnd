703a1eb868c3d78518c1f10fac1714c23f619900
Alcrro <alex.roventa94@gmail.com>
2022-11-05 15:45:49 +0200

    main

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const server = express();
 
-server.listen(3000);
+server.get('/', (req, res, next) => {
+  res.json({
+    success: true,
+  });
+});
+
+server.listen(5000);
