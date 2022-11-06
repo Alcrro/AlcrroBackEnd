2ba6bd702bca2e2129d8fbed8482028103b433a1
Alcrro <alex.roventa94@gmail.com>
2022-11-06 09:00:36 +0200

    main

--- frontEnd/frontend.js
+++ frontEnd/frontend.js
@@ -8,12 +8,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, './public')));
 
-const viewsPath = path.join(__dirname, './public/');
+const viewsPath = path.join(__dirname, './public/views');
 app.set('views', viewsPath);
 
 app.get('/', (req, res, next) => {
-  res.render('views/index');
-  console.log('S-a incarcat cu success');
+  res.render('views/index', {
+    pageTitle: 'ALCRRO.RO',
+  });
 });
 
 app.listen(3000);
